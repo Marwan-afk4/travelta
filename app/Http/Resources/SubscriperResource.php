7715f102aac44f33419b08a250e7894ac51a1524
Ahmed <Wegostationdev@gmail.com>
2025-01-09 10:22:34 +0200

    commit

--- app/Http/Resources/SubscriperResource.php
+++ app/Http/Resources/SubscriperResource.php
@@ -14,6 +14,16 @@ class SubscriperResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        $email = null;
+        $phone = null;
+        if (isset($this->to_client->emails)) {
+            $email = json_decode($this->to_client->emails) ?json_decode($this->to_client->emails)[0]: $this->to_client->emails;
+            $phone = json_decode($this->to_client->phones) ?json_decode($this->to_client->phones)[0]: $this->to_client->phones;
+        }
+        else{
+            $email = $this->to_client->email;
+            $phone = $this->to_client->phone;
+        }
         return [
             'supplier_from_name' => $this->from_supplier->agent ?? null,
             'supplier_from_email' => json_decode($this->emails) ?? $this->emails,
@@ -22,8 +32,8 @@ class SubscriperResource extends JsonResource
             'total_price' => $this->total_price ?? null,
             'to_name' => $this->to_client->name ?? null,
             'to_role' => isset($this->to_client->agent) ? 'Supplier' : 'Customer',
-            'to_email' => isset($this->to_client->emails) ? (is_string($this->to_client->emails) ? (json_decode($this->to_client->emails)[0]) : $this->to_client->emails): $this->to_client->email,
-            'to_phone' => isset($this->to_client->phones) ? (is_string($this->to_client->phones) ? (json_decode($this->to_client->phones)[0]) : $this->to_client->phones): $this->to_client->phone,
+            'to_email' => $email,
+            'to_phone' => $phone,
         ];
     }
 }
