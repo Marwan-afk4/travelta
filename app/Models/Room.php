54cc29053447bb14c0ed26ba2b09bd478bae33c7
Ahmed <Wegostationdev@gmail.com>
2025-01-12 16:10:00 +0200

    commit

---
+++ app/Models/Room.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Room extends Model
+{
+    protected $fillable = [
+        'description',
+        'status',
+        'price_type',
+        'price',
+        'quantity',
+        'max_adults',
+        'max_children',
+        'max_capacity',
+        'min_stay',
+        'room_type_id',
+        'hotel_id',
+        'hotel_meal_id',
+        'currency_id',
+        'b2c_markup',
+        'b2e_markup',
+        'b2b_markup',
+        'tax_type',
+        'check_in',
+        'check_out',
+        'policy',
+        'children_policy',
+        'cancelation',
+    ];
+}
---
+++ database/migrations/2025_01_12_135758_create_rooms_table.php
@@ -0,0 +1,49 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rooms', function (Blueprint $table) {
+            $table->id();
+            $table->text('description')->nullable();
+            $table->boolean('status')->default(1);
+            $table->enum('price_type', ['fixed', 'variable']);
+            $table->float('price')->nullable();
+            $table->integer('quantity');
+            $table->integer('max_adults');
+            $table->integer('max_children');
+            $table->integer('max_capacity');
+            $table->integer('min_stay');
+            $table->foreignId(column: 'room_type_id')->nullable()->constrained('room_types')->onUpdate('cascade')->onDelete('set null');
+            $table->foreignId(column: 'hotel_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('set null');
+            $table->foreignId(column: 'hotel_meal_id')->nullable()->constrained('hotel_meals')->onUpdate('cascade')->onDelete('set null');
+            $table->foreignId(column: 'currency_id')->nullable()->constrained('currency_agents')->onUpdate('cascade')->onDelete('set null');
+            $table->float('b2c_markup');
+            $table->float('b2e_markup');
+            $table->float('b2b_markup');
+            $table->enum('tax_type', ['include', 'exclude', 'include_except']);
+            $table->string('check_in');
+            $table->string('check_out');
+            $table->text('policy');
+            $table->text('children_policy');
+            $table->enum('cancelation', ['free', 'non_refunable']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rooms');
+    }
+};
