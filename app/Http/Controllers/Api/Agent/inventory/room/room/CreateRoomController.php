7802546ed764a1291bfc70075a7423f25cdb34de
Ahmed <Wegostationdev@gmail.com>
2025-01-12 17:02:05 +0200

    make lists at room

---
+++ app/Http/Controllers/Api/Agent/inventory/room/room/CreateRoomController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Api\Agent\inventory\room\room;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class CreateRoomController extends Controller
+{
+    //
+}
---
+++ app/Http/Controllers/Api/Agent/inventory/room/room/RoomController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\Api\Agent\inventory\room\room;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use App\Models\RoomType;
+use App\Models\Hotel;
+use App\Models\HotelMeal;
+use App\Models\CurrencyAgent;
+use App\Models\RoomAmenity;
+use App\Models\CountryTax;
+
+class RoomController extends Controller
+{
+    public function __construct(private RoomType $room_types, private Hotel $hotels,
+    private HotelMeal $meal_plans, private CurrencyAgent $currencies, 
+    private RoomAmenity $room_amenities, private CountryTax $country_taxes){}
+
+    public function lists(Request $request){
+        if ($request->user()->affilate_id && !empty($request->user()->affilate_id)) {
+            $agent_id = $request->user()->affilate_id;
+        }
+        elseif ($request->user()->agent_id && !empty($request->user()->agent_id)) {
+            $agent_id = $request->user()->agent_id;
+        }
+        else{
+            $agent_id = $request->user()->id;
+        }
+        if ($request->user()->role == 'affilate' || $request->user()->role == 'freelancer') {
+            $role = 'affilate_id';
+        }
+        else{
+            $role = 'agent_id';
+        }
+        $hotels = $this->hotels
+        ->get();
+        $room_types = $this->room_types
+        ->where($role, $agent_id)
+        ->get();
+        $currencies = $this->currencies
+        ->where($role, $agent_id)
+        ->get();
+
+        return response()->json([
+            'hotels' => $hotels,
+            'room_types' => $room_types,
+            'currencies' => $currencies,
+        ]);
+    }
+
+    public function hotel_lists(){
+
+    }
+}
