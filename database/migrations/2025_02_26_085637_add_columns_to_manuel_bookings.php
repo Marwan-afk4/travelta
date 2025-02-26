6d6209739a80d2c3f1f92aabc7d74d6e6a7591ec
Ahmed <Wegostationdev@gmail.com>
2025-02-26 15:27:28 +0200

    commit

--- database/migrations/2025_02_26_085637_add_columns_to_manuel_bookings.php
+++ database/migrations/2025_02_26_085637_add_columns_to_manuel_bookings.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('manuel_bookings', function (Blueprint $table) {
-            $table->string('voucher')->after('invoice')->nullable();
+            $table->string('voucher')->after('id')->nullable();
         });
     }
 
