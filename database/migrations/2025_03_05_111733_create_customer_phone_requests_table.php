c87f9422e604f341ea052ee128d1f1853b921c16
Ahmed <Wegostationdev@gmail.com>
2025-03-05 13:29:56 +0200

    fdg

--- database/migrations/2025_03_05_111733_create_customer_phone_requests_table.php
+++ database/migrations/2025_03_05_111733_create_customer_phone_requests_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->foreignId('agent_id')->nullable()->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->string('old_phone');
             $table->string('new_phone');
-            $table->boolean('status');
+            $table->boolean('status')->default(0);
             $table->timestamps();
         });
     }
