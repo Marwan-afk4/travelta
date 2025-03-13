165bd0d467c1daa8859e669a6cdfc49fcc8ac258
Ahmed <Wegostationdev@gmail.com>
2025-03-13 14:40:26 +0200

    commit

--- app/Http/Resources/ManuelHotelResource.php
+++ app/Http/Resources/ManuelHotelResource.php
@@ -25,8 +25,9 @@ class ManuelHotelResource extends JsonResource
             'total_price' => number_format($this->total_price, 2, '.', ''),
             'to_name' => $this->to_client->name ?? null,
             'to_role' => isset($this->to_client->agent) ? 'Supplier' : 'Customer',
-            'to_email' => $this->to_client->emails ? $this->to_client->emails[0]: $this->to_client->email ?? null,
-            'to_phone' => $this->to_client->phones ? strval($this->to_client->phones[0]): $this->to_client->phone ?? null,
+            'to_email' => isset($this->to_client->emails) ? $this->to_client->emails[0]: 
+            $this->to_client->email ?? null,
+            'to_phone' => isset($this->to_client->phones) ? strval($this->to_client->phones[0]): $this->to_client->phone ?? null,
             'hotel_name' => $this->hotel->hotel_name ?? null,
             'check_in' => $this->hotel->check_in ?? null,
             'check_out' => $this->hotel->check_out ?? null,
