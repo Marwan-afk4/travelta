291cbb6c9d3654f3cc3608ab6b1a0bde6330fe63
Ahmed <Wegostationdev@gmail.com>
2025-04-15 11:39:23 +0200

    commit

--- app/Http/Resources/Accounting/PaymentReceivableResource.php
+++ app/Http/Resources/Accounting/PaymentReceivableResource.php
@@ -32,7 +32,7 @@ class PaymentReceivableResource extends JsonResource
             'manuel_code' => $this->code,
             'created' => $this->created_at->format('Y-m-d'),
             'type' => empty($this->to_supplier_id) ? 'Customer': 'Supplier',
-            'client_name' => $this->to_client->name,
+            'client_name' => $this?->to_client?->name ?? null,
             'client_phone' => empty($this->to_supplier_id) ? $this->to_client->phone : $this->to_client->phones[0] ?? $this->to_client->phones,
             'total' => $this->total_price,
             'paid' => $this->payments->sum('amount'),
