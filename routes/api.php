a3d2181b18d11d04fee074e7f6c450f8cf089c1f
Ahmed <Wegostationdev@gmail.com>
2025-06-24 10:00:52 +0200

    commit

--- routes/api.php
+++ routes/api.php
@@ -56,9 +56,9 @@ Route::middleware(['auth:sanctum','IsSuperAdmin'])->group(function () {
 
         Route::get('/super/meal_plan',[MealPlanController::class,'view']);
         
-        Route::get('/super/meal_plan',[MealPlanController::class,'meal']);
+        Route::get('/super/meal_plan/item/{id}',[MealPlanController::class,'meal']);
 
-        Route::get('/super/meal_plan',[MealPlanController::class,'status']);
+        Route::get('/super/meal_plan/status/{id}',[MealPlanController::class,'status']);
 
         Route::post('/super/meal_plan/add',[MealPlanController::class,'create']);
 
