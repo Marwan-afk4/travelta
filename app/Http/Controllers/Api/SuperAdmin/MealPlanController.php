af0b00874e8800cc2b733f2b8345ce409d8595e2
Ahmed <Wegostationdev@gmail.com>
2025-06-24 10:06:41 +0200

    commit

--- app/Http/Controllers/Api/SuperAdmin/MealPlanController.php
+++ app/Http/Controllers/Api/SuperAdmin/MealPlanController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api\SuperAdmin;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 
 use App\Models\HotelMeal;
