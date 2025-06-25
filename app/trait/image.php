574f02e70cf206af71d1a4d0f0474fb3ed62bfdb
Ahmed <Wegostationdev@gmail.com>
2025-06-25 12:56:34 +0200

    commit

--- app/trait/image.php
+++ app/trait/image.php
@@ -61,13 +61,12 @@ trait image
 
     public function deleteImage($imagePath){
         // Check if the file exists
-        if ($imagePath && Storage::disk('public')->exists($imagePath)) {
-            try {
+        try {
+            if ($imagePath && Storage::disk('public')->exists($imagePath)) {
                 Storage::disk('public')->delete($imagePath);
-            } 
-            catch (\Throwable $th) {
-                //throw $th;
             }
+        } catch (\Throwable $th) {
+            //throw $th;
         }
     }
 
