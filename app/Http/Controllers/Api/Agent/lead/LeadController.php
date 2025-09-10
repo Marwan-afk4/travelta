23227e4c55534842d05b4a0fb150b1c11a723ce0
Ahmed <Wegostationdev@gmail.com>
2025-09-10 14:43:43 +0200

    commit

--- app/Http/Controllers/Api/Agent/lead/LeadController.php
+++ app/Http/Controllers/Api/Agent/lead/LeadController.php
@@ -504,7 +504,7 @@ class LeadController extends Controller
             'nationality_id' => $request->nationality_id ?? null,
             'country_id' => $request->country_id ?? null,
             'city_id' => $request->city_id ?? null,
-            'status' => $request->status ?? null,
+            'status' => $request->status ?? 'lead',
             'image' => $customer->image ?? null,
         ];
         if ($request->affilate_id) {
